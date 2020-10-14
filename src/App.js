92de726cc7e03482f5c5640a2152263bd6eb6a77
root <exploer.ctu.dev@gmail.com>
2020-10-14 23:33:59 +0900

    props

--- src/App.js
+++ src/App.js
@@ -27,22 +27,36 @@ import './App.css';
 // }
 
 const App = () =>{
+
+  const profiles = [
+    {
+      name: "Taro", age:10
+    },
+    {
+      name: "Hanako", age:5
+    },
+    { name: "NoAge"}
+  ]
+
   return (
-  <div>
-    <Cat/>
-    <Cat/>
-    <Cat/>
-    <Cat/>
-    <Cat/>
-    <Cat/>
-  </div>
+    <React.Fragment>
+    {
+      profiles.map((profile,index ) => {
+        return <User name={profile.name} age={profile.age} key={index}/>
+      })
+    }
+  </React.Fragment>
   )
 }
 
-const Cat = () =>{
-  return <div>Meow!</div>
+const User = (props) =>{
+  return <div>Hi!, I am {props.name}! and {props.age} years old!</div>
 }
 
+
+User.defaultProps = {
+  age: 1
+}
 
 
 export default App;
