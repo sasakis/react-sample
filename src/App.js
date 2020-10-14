710d73ffa45ff5b259b6cfe23353217a20828bce
root <exploer.ctu.dev@gmail.com>
2020-10-14 23:22:42 +0900

    mod

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Component} from 'react';
-import logo from './logo.svg';
+//import { Component} from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
 // class App extends Component {
